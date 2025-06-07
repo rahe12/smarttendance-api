51a7b0a162fef3e919b357e1a2465d83f91a5c8d
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-06-07 19:11:03 +0200

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const studentCtrl = require('../controllers/studentController');
 
+// Student routes
 router.post('/', studentCtrl.addStudent);
 router.get('/', studentCtrl.getStudents);
 router.put('/:id', studentCtrl.updateStudent);
 router.delete('/:id', studentCtrl.deleteStudent);
+
+// People management routes
+router.post('/people', studentCtrl.addPerson);
+router.get('/people', studentCtrl.getPeople);
+router.get('/people/:id', studentCtrl.getPersonById);
+router.put('/people/:id', studentCtrl.updatePerson);
+router.delete('/people/:id', studentCtrl.deletePerson);
 
 module.exports = router;
